1282b5c45a48b2b83bf5f0880916193cef4e0379
Alexander Shilenkov <85848281+Alex-esc1@users.noreply.github.com>
2022-04-03 01:09:06 +0300

    import path for webpack
    импортирую библиотеку path для вебпак, далее указываю путь для сборки, также для имя файла буду использовать регулярные выражения, которые позволят избавиться с проблемами хеширования при разработке. То есть вебпак будет отслеживать при сборке измененные js файлы и делать уникальные названия

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
-
+const path = require ('path')
 
 module.export = {
     mode: "development",
     entry: ".src/index.js",
     oytput: {
-
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].[hash].js"
     }
 }
