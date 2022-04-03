3acad66a02c8a8ebc358acda1b786ec28357b739
Alexander Shilenkov <85848281+Alex-esc1@users.noreply.github.com>
2022-04-03 21:04:25 +0300

    Связь React + Redux
    Связываю React приложение c Redux + проверка работы (использую хуки: useDispatch для получения функции store.dispatch в компоненте + useSelector для вывода состояния на фронт часть)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,8 +5,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
-    repos: reposReducer,
-
+    repos: reposReducer
 })
 
 export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
