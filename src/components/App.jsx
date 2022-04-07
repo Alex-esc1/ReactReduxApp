5b27459f224e7c1a93dc7288d8ad71b7924300b5
Alexander Shilenkov <85848281+Alex-esc1@users.noreply.github.com>
2022-04-07 14:29:16 +0300

    Создание основных компонентов приложения
    BrowserRouter - необходимо для перемещения по страницам

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import './app.less'
 import {useDispatch, useSelector} from "react-redux";
-import {setCount} from "../reducers/reposReducer";
+import {BrowserRouter} from "react-router-dom";
+
+
 
 const App = () => {
     const dispatch = useDispatch()
-    const count = useSelector(state => state.repos.count)
-
-    function onCountClick() {
-        dispatch(setCount(5))
-    }
 
     return (
-        <div className="app">
-            <button onClick={()=>onCountClick()}>COUNT</button>
-            <div>{count}</div>
-        </div>
+        <BrowserRouter>
+
+        </BrowserRouter>
     );
 };
 
