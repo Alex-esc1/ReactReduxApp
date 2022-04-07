b6f74e2684312db9ae0e5bdba69377e2da793862
Alexander Shilenkov <85848281+Alex-esc1@users.noreply.github.com>
2022-04-07 15:37:47 +0300

    useDispatch, useEffect, useSelector

--- src/components/main/repo/Repo.jsx
+++ src/components/main/repo/Repo.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import './repo.less';
+import './repo.less'
+
 
 const Repo = (props) => {
     const repo = props.repo
 
-
     return (
-        <div>
-
+        <div className="repo">
+            <div className="repo-header">
+                <div className="repo-header-name">{repo.name}</div>
+                <div className="repo-header-stars">Количество звезд: {repo.stargazers_count}</div>
+            </div>
+            <div className="repo-last-commit">Последний коммит: {repo.updated_at}</div>
+            <a href={repo.html_url} className="repo-link">Ссылка на репозиторий: {repo.html_url}</a>
         </div>
     );
 };
