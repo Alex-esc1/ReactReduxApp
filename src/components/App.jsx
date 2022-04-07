e73de6adf36b817ee1467f7d3fe9521d3b6fbf2d
Alexander Shilenkov <85848281+Alex-esc1@users.noreply.github.com>
2022-04-07 14:33:56 +0300

    Создал первый Router + задаю стиль вывода
    Принимает 2 параметра: компонент и путь по которому данный компонент будет вызываться. Для контейнера в стилях задаю фиксированную ширину

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import './app.less'
 import {useDispatch, useSelector} from "react-redux";
 import {BrowserRouter} from "react-router-dom";
-
+import {Route} from "react-router";
+import Main from "./main/Main.";
 
 
 const App = () => {
@@ -10,7 +11,9 @@ const App = () => {
 
     return (
         <BrowserRouter>
-
+            <div className="container">
+                <Route path="/" component={Main} />
+            </div>
         </BrowserRouter>
     );
 };
